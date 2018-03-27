fcd77008ac4e2b3181367dda17f11f36adf78669
SirG9 <37812413+SirG9@users.noreply.github.com>
2018-03-27 17:00:04 +0530

    Add files via upload

---
+++ src/com/chicho/java/json2javaobject/JsonToJavaObj.java
@@ -0,0 +1,18 @@
+package com.chicho.java.jsontojava;
+
+import com.chicho.json.CBPMarineLubeChartIntJSONResponseDTO;
+import com.chicho.json.CBPMarineLubeChartIntJSONResultDTO;
+import com.google.gson.Gson;
+
+public class JsonToJavaObj {
+
+	public static void main(String[] args) {
+		String restResp = "{'IsSuccess': true,'Result': [{'IMO': 9118630,'MPIds': '219000,219010'},{'IMO': 7827029,'MPIds': '219574'}]}";
+		CBPMarineLubeChartIntJSONResponseDTO jsonResponse = new Gson().fromJson(restResp,CBPMarineLubeChartIntJSONResponseDTO.class);
+		System.out.println(jsonResponse.toString());
+		
+		for (CBPMarineLubeChartIntJSONResultDTO listOfProd : jsonResponse.getResult()) {
+			listOfProd.setMPIdsList(listOfProd.getMPIds().split(","));
+		}
+	}
+}
