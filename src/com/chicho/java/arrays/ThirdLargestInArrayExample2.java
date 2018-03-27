912b50ae5d187c3e91f13aeaa5bc66094ed2a4f7
SirG9 <37812413+SirG9@users.noreply.github.com>
2018-03-27 23:41:49 +0530

    Various Array Example added

---
+++ src/com/chicho/java/arrays/ThirdLargestInArrayExample2.java
@@ -0,0 +1,20 @@
+package com.chicho.java.arrays;
+
+import java.util.*;
+
+/*Find 3rd Largest Number in Array using Collections*/
+public class ThirdLargestInArrayExample2 {
+	public static int getThirdLargest(Integer[] a, int total) {
+		List<Integer> list = Arrays.asList(a);
+		Collections.sort(list);
+		int element = list.get(total - 3);
+		return element;
+	}
+
+	public static void main(String args[]) {
+		Integer a[] = { 1, 2, 5, 6, 3, 2 };
+		Integer b[] = { 44, 66, 99, 77, 33, 22, 55 };
+		System.out.println("Third Largest: " + getThirdLargest(a, 6));
+		System.out.println("Third Largest: " + getThirdLargest(b, 7));
+	}
+}
