141fd67b21457286868cb1614ce287fd25a3491c
SirG9 <37812413+SirG9@users.noreply.github.com>
2018-03-28 17:38:52 +0530

    Add files via upload

---
+++ src/com/chicho/java/jsontojava/CBPMarineLubeChartIntJSONResultDTO.java
@@ -0,0 +1,31 @@
+package com.chicho.json;
+
+public class CBPMarineLubeChartIntJSONResultDTO {
+	private String IMO;
+	private String MPIds;
+	private String[] MPIdsList;
+
+	public String getIMO() {
+		return IMO;
+	}
+
+	public void setIMO(String iMO) {
+		IMO = iMO;
+	}
+
+	public String getMPIds() {
+		return MPIds;
+	}
+
+	public void setMPIds(String mPIds) {
+		MPIds = mPIds;
+	}
+
+	public String[] getMPIdsList() {
+		return MPIdsList;
+	}
+
+	public void setMPIdsList(String[] mPIdsList) {
+		MPIdsList = mPIdsList;
+	}
+}
