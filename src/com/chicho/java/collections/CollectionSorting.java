95fea750746d9d23ab0892070ad01e12d2c8f30a
SirG9 <37812413+SirG9@users.noreply.github.com>
2018-03-31 00:03:50 +0530

    ArrayList is sorted in Ascending/Descending successful using collections

---
+++ src/com/chicho/java/collections/CollectionSorting.java
@@ -0,0 +1,33 @@
+package com.chicho.java.collections;
+
+/* Java program to demonstrate working of Collections.sort() to descending order. */
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CollectionSorting {
+	public static void main(String[] args) {
+		// Create a list of strings
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("Geeks For Geeks");
+		al.add("Friends");
+		al.add("Dear");
+		al.add("Is");
+		al.add("Superb");
+
+		/*
+		 * Collections.sort method is sorting the elements of ArrayList in
+		 * ascending order.
+		 */
+		Collections.sort(al);
+
+		/*
+		 * Collections.sort method is sorting the elements of ArrayList in
+		 * descending order.
+		 */
+		Collections.sort(al, Collections.reverseOrder());
+
+		// Let us print the sorted list
+		System.out.println("List after the use of" + " Collection.sort() :\n" + al);
+	}
+}
