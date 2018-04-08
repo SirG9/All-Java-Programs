7d8725544086087c44f13c7ef15dd412ddd96e35
SirG9 <37812413+SirG9@users.noreply.github.com>
2018-04-08 11:44:03 +0530

    LinkedList Class and Priority Queue Class Java Example Code Added

---
+++ src/com/chicho/java/collections/LinkedListAdd.java
@@ -0,0 +1,22 @@
+package com.chicho.java.collections;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListAdd {
+
+	public static void main(String[] args) {
+		LinkedList<String> al = new LinkedList<String>();
+		al.add("Ravi");
+		al.add("Vijay");
+		al.add("Ravi");
+		al.add("Ajay");
+
+		Iterator<String> itr = al.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+	}
+
+}
