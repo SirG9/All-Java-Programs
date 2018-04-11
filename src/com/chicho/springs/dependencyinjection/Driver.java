05239a1f5271d6a5233c6be03cc74e757c7c819b
CH318977 <CH318977@D-113102958.wipro.com>
2018-04-11 11:07:20 +0530

    Springs: Constructor Injection with Primitive/Dependent Object

---
+++ src/com/chicho/springs/dependencyinjection/Driver.java
@@ -0,0 +1,16 @@
+package com.chicho.springs.dependencyinjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Driver {
+	public static void main(String[] args) {
+
+		ApplicationContext ac = new ClassPathXmlApplicationContext(
+				"com/chicho/springs/dependencyinjection/applicationContext.xml");
+
+		Employee emp = (Employee) ac.getBean("e");
+		emp.show();
+
+	}
+}
