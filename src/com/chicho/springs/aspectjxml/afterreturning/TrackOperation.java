c2355b5a475f85283814f33311493117a786144b
CH318977 <CH318977@D-113102958.wipro.com>
2018-04-13 13:00:57 +0530

    Spring AspectJ using XML example code added

---
+++ src/com/chicho/springs/aspectjxml/afterreturning/TrackOperation.java
@@ -0,0 +1,14 @@
+package com.chicho.springs.aspectjxml.afterreturning;
+
+import org.aspectj.lang.JoinPoint;
+
+public class TrackOperation {
+	public void myadvice(JoinPoint jp, Object result)// it is advice (after
+														// advice)
+	{
+		System.out.println("additional concern");
+		System.out.println("Method Signature: " + jp.getSignature());
+		System.out.println("Result in advice: " + result);
+		System.out.println("end of after returning advice...");
+	}
+}
