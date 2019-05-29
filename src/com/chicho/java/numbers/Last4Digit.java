3b9d6c2a24250fd366e8c3c2c27a0032c20d13cf
csandip <csandip@deloitte.com>
2019-05-29 11:27:24 +0530

    Credit Card Last 4 Digit and BIN Number code

---
+++ src/com/chicho/java/numbers/Last4Digit.java
@@ -0,0 +1,13 @@
+package com.chicho.java.numbers;
+
+public class Last4Digit {
+
+	public static void main(String[] args) {
+		String cardNumber = "1234567890123456";
+		String last4Digits = cardNumber != null ? cardNumber.subSequence(cardNumber.length() >= 4 ? cardNumber.length() - 4 : 0, cardNumber.length()).toString():null;
+		System.out.println("Last4Digit: "+last4Digits);
+		
+		System.out.println("Bin Number: "+cardNumber.substring(0,6));
+	}
+
+}
